feb401199b86d0f8c71b8c767a145bc840752338
Bikram <bikram@FH-MAC-19-055.local>
2020-01-27 16:33:26 +0530

    background added for the file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import Shelf from './Components/Shelf';
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">
+    <Shelf />
+  </div>;
 }
 
 export default App;
---
+++ src/Components/Shelf/Shelf.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Shelf = () => <div> Shelf component</div>;
+
+export default Shelf;
